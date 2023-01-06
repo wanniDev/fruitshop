92d5a7e5db4b4ff0cc83a45341d5e34b10b1c31e
wannidev0928 <wannidev0928@gmail.com>
2023-01-06 16:08:13 +0900

    Feat: 스마트컨트랙트 생성 및 관리를 위해 ganache 구성 및 web3j 연동

---
+++ src/main/kotlin/me/ethereum/fruit/config/ContractConfig.kt
@@ -0,0 +1,14 @@
+package me.ethereum.fruit.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+
+@Configuration
+class ContractConfig {
+    @Bean
+    fun web3j(): Web3j {
+        return Web3j.build(HttpService())
+    }
+}
